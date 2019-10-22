3d37bd9a8ab230b40aa0c8d6c5d00fd7450001ee
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:45:38 +0200

    il terzo esempio è servito a far vedere un opportuno messaggio in console solo  dopo che tutti i worker thread abbiano finito la loro esecuzione

--- esercitazione thread/Program.cs
+++ esercitazione thread/Program.cs
@@ -11,27 +11,58 @@ namespace esercitazione_thread
     {
         static void Main(string[] args)
         {
-            Thread myThread = new Thread(() =>
+            //Thread myThread = new Thread(() =>
+            //{
+            //    Console.WriteLine("MyThread è iniziato");
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("MyThread è terminato");
+            //});
+
+            ////esecuzione di mythread
+            //myThread.Start();
+
+            //Thread.Sleep(500);
+            //Console.WriteLine("main thread");
+
+
+            //Console.WriteLine();
+
+
+            //string someVariabile = "matteo Tumiati";
+
+            //var workerThread = new Thread((o) =>
+            // {
+            //     Console.WriteLine("saluti da :{0}", someVariabile);
+            // });
+
+
+            //Console.WriteLine();
+
+
+
+            var list = new List<Thread>();
+            //qui creiamo ed eseguiamo cinque worker thread 
+            for (int index = 0 ; index < 5 ; index++)
             {
-                Console.WriteLine("MyThread è iniziato");
-                Thread.Sleep(1000);
-                Console.WriteLine("MyThread è terminato");
-            });
+                var myThread = new Thread((currentIndex) =>
+               {
+                   Console.WriteLine("thread {0} è iniziato", currentIndex);
+                   Thread.Sleep(500);
+                   Console.WriteLine("thread {0} è terminato", currentIndex);
+               });
+                myThread.Start(index);
+                list.Add(myThread);
+            }
+            //attesa  del completamento di ognuno dei worker thread 
+            foreach(Thread thread in list)
+            {
+                thread.Join();
+            }
+            Console.WriteLine("esecuzione di tutti i therad terminata");
 
-            //esecuzione di mythread
-            myThread.Start();
 
-            Thread.Sleep(500);
-            Console.WriteLine("main thread");
 
-            Console.WriteLine();
 
-            string someVariabile = "matteo Tumiati";
-
-            var workerThread = new Thread((o) =>
-             {
-                 Console.WriteLine("saluti da :{0}", someVariabile);
-             });
 
             Console.ReadLine();
         }
