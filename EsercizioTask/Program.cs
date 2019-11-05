e105255e6497a5449f41148be0c28560b58f2103
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 09:50:16 +0100

    inizio esercizio task

---
+++ EsercizioTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EsercizioTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //costruzione di un semplice task 
+            var simpleTask = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("ciao da simpleTask");
+            });
+
+            //costruzione di un task con parametro in input
+            var parameterTask = Task.Factory.StartNew(name)=>
+                
+        }
+    }
+}
